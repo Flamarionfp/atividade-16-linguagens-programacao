0632a29eb61d15639539edc5695aec5427105866
flamarionfp <flamarionpinto@sou.faccat.br>
2023-06-24 14:27:50 -0300

    feat: finish

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import fastify from "fastify";
 import { visitorsRoutes } from "./routes/visitors";
+import cors from "@fastify/cors";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: "http://localhost:3000",
+  methods: ["GET", "PATCH"],
+});
 
 app.register(visitorsRoutes);
 
